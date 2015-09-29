ef3e3891215acfe06057795739b3869c55474f6b
Mark Salib <marksalib2@gmail.com>
2015-09-30 00:01:48 +0100

    image fade on services page

--- script.js
+++ script.js
@@ -608,12 +608,24 @@ $(function () {
     displayWorks();
     sortByTitle();
     accordions();
-
+    setInterval('cycleImages()', 5000);
 });
 
 //everything out of here isn't ON LOAD.//
 
 //Accordion sliders on about and services page//
+
+function cycleImages(){
+      var $active = $('#cycler .active');
+      var $next = ($active.next().length > 0) ? $active.next() : $('#cycler img:first');
+      $next.css('z-index',2);//move the next image up the pile
+      $active.fadeOut(1500,function(){//fade out the top image
+	  $active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
+          $next.css('z-index',3).addClass('active');//make the next image the top one
+      });
+    }
+
+
 
 $('#searchWorks').value()
 
