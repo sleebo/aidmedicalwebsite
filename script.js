5ddfccd2a0b41c504597eab15dc13c9648c93078
Mark Salib <marksalib2@gmail.com>
2015-09-30 20:40:50 +0100

    adding images

--- script.js
+++ script.js
@@ -608,7 +608,7 @@ $(function () {
     displayWorks();
     sortByTitle();
     accordions();
-    setInterval('cycleImages()', 5000);
+    setInterval('cycleImages()', 7000);
 });
 
 //everything out of here isn't ON LOAD.//
@@ -619,7 +619,7 @@ function cycleImages(){
       var $active = $('#cycler .active');
       var $next = ($active.next().length > 0) ? $active.next() : $('#cycler img:first');
       $next.css('z-index',2);//move the next image up the pile
-      $active.fadeOut(1500,function(){//fade out the top image
+      $active.fadeOut(2500,function(){//fade out the top image
 	  $active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
           $next.css('z-index',3).addClass('active');//make the next image the top one
       });
