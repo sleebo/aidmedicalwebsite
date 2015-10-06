9c74403ae2c6f2d4c0199cd438a31a1679ee1bf0
Mark Salib <marksalib2@gmail.com>
2015-10-06 09:51:38 +0100

    checkign domain change works

--- script.js
+++ script.js
@@ -615,12 +615,46 @@ function autoCompleteArticles() {
    
      
     ];
-     
-    $( "#search" ).catcomplete({
-      delay: 0,
-      source: data
-    });
-  };
+//     
+//    $( "#search" ).catcomplete({
+//      delay: 0,
+//      source: data
+//    });
+//  };
+
+
+    var searchedElement = $('#search').val ;
+    
+    function searchDadsWorks(){
+        
+    }
+
+    
+    
+$.widget( "custom.catcomplete", $.ui.autocomplete, {
+    _create: function() {
+      this._super();
+      this.widget().menu( "option", "items", "> :not(.ui-autocomplete-category)" );
+    },
+    _renderMenu: function( ul, items ) {
+      var that = this,
+        currentCategory = "";
+      $.each( items, function( index, item ) {
+        var li;
+        if ( item.category != currentCategory ) {
+          ul.append( "<li class='ui-autocomplete-category'>" + item.category + "</li>" );
+          currentCategory = item.category;
+        }
+        li = that._renderItemData( ul, item );
+        if ( item.category ) {
+          li.attr( "aria-label", item.category + " : " + item.label );
+        }
+      });
+    }
+  });
+
+ 
+
 
 
 //When Page Loads, Execute THIS code//
@@ -659,31 +693,6 @@ $(function () {
 //      });
 //    }
 //
-
-
-$.widget( "custom.catcomplete", $.ui.autocomplete, {
-    _create: function() {
-      this._super();
-      this.widget().menu( "option", "items", "> :not(.ui-autocomplete-category)" );
-    },
-    _renderMenu: function( ul, items ) {
-      var that = this,
-        currentCategory = "";
-      $.each( items, function( index, item ) {
-        var li;
-        if ( item.category != currentCategory ) {
-          ul.append( "<li class='ui-autocomplete-category'>" + item.category + "</li>" );
-          currentCategory = item.category;
-        }
-        li = that._renderItemData( ul, item );
-        if ( item.category ) {
-          li.attr( "aria-label", item.category + " : " + item.label );
-        }
-      });
-    }
-  });
-
- 
 
 
 
